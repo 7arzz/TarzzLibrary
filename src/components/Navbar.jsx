cc6e80b01a7984e01caf59805416ee4157565b27
7arzz <7arzz404@gmail.com>
2026-02-15 20:11:41 +0700

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,25 +5,42 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark sticky-top">
+    <nav className="navbar navbar-expand-lg navbar-dark  sticky-top">
       <div className="container">
-        <Link className="navbar-brand d-flex align-items-center gap-2" to="/">
-          <img src={logo} width="50" alt="logo" className="rounded-circle" />
+        {/* Logo */}
+        <Link
+          className="navbar-brand d-flex align-items-center gap-2 glow"
+          to="/"
+        >
+          <img src={logo} width="45" alt="logo" className="rounded-circle" />
           Tarzz Library
         </Link>
 
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ms-auto gap-3">
+        {/* Button hamburger (HP) */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Menu */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto gap-lg-3 text-center">
             <li className="nav-item">
               <Link className="nav-link" to="/">
                 Home
               </Link>
             </li>
+
             <li className="nav-item">
               <Link className="nav-link" to="/library">
                 Library
               </Link>
             </li>
+
             <li className="nav-item">
               <Link className="nav-link" to="/about">
                 About
